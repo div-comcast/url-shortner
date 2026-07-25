4ab19735af8ab430580d189d0ef907b21436eaa7
div-comcast <Divyanshu_Divyanshu@comcast.com>
2026-07-25 18:05:14 +0530

    Cors in main

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.router import router
 
 app = FastAPI(title="URL Shortener")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:4000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(router)
 
 
