60a77603a11b36f3fe6c547c554eda9e95ac5f0b
div-comcast <Divyanshu_Divyanshu@comcast.com>
2026-07-25 20:37:55 +0530

    router.py

--- backend/app/api/router.py
+++ backend/app/api/router.py
@@ -4,5 +4,13 @@ from app.api.endpoints.apis import router as url_router
 from app.api.endpoints.health import router as health_router
 
 router = APIRouter()
-router.include_router(health_router)
-router.include_router(url_router)
+
+router.include_router(
+    health_router,
+    tags=["Health"]
+)
+
+router.include_router(
+    url_router,
+    tags=["URLs"]
+)
